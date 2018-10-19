0c5428ac3f96eef02804c5924038def7ead35f8b
ovacia345 <nielsvann@hotmail.nl>
2018-10-19 15:14:55 +0200

    Add adjacencyLists and addEdge() to Graph.java

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -1,10 +1,24 @@
 package graphs;
 
+import java.util.List;
+
 /**
  *
  * @author N.C.M. van Nistelrooij
  * @author C Amghane
  */
 public class Graph<T> {
+    private final int nrVertices;
+    private final List<Integer>[] adjacencyLists;
 
+
+    public Graph(int nrVertices) {
+        this.nrVertices = nrVertices;
+
+        adjacencyLists = new List[nrVertices];
+    }
+
+    public void addEdge(int source, int destination) {
+        adjacencyLists[source].add(destination);
+    }
 }
