c56386f1ce9497e31d6e62a93d925cec4d6f335d
ovacia345 <nielsvann@hotmail.nl>
2018-10-24 20:20:21 +0200

    Add checks for parentEdges parameter in getPath()

--- src/graphs/BFS.java
+++ src/graphs/BFS.java
@@ -16,9 +16,9 @@ public class BFS {
 
     public static <T extends Number> Edge<T>[] run(Graph<T> graph, int source) {
         checkNullGraph(graph);
-        checkInvalidVertex(graph, source);
-
         int nrVertices = graph.getNrVertices();
+        checkInvalidVertex(nrVertices, source);
+
         Color[] colors = new Color[nrVertices];
         Edge<T>[] parentEdges = new Edge[nrVertices];
 
@@ -32,13 +32,14 @@ public class BFS {
 
         while(!queue.isEmpty()) {
             int vertexU = queue.remove();
-            List<Edge<T>> adjacencyListU = graph.getAdjacencyList(vertexU);
+            List<Edge<T>> adjacencyListVertexU = graph.getAdjacencyList(vertexU);
 
-            for(Edge<T> edgeUV : adjacencyListU) {
+            for(Edge<T> edgeUV : adjacencyListVertexU) {
                 int vertexV = edgeUV.getDestination();
                 if(colors[vertexV] == Color.WHITE) {
                     colors[vertexV] = Color.GRAY;
                     parentEdges[vertexV] = edgeUV;
+                    queue.add(vertexV);
                 }
             }
 
@@ -49,30 +50,45 @@ public class BFS {
     }
 
     public static <T extends Number> List<Edge<T>> getPath(Edge<T>[] parentEdges,
-            int child) {
-        return getPathRecursive(parentEdges, child, 0);
+            int lastVertex) {
+        int nrVertices = parentEdges.length;
+
+        checkInvalidVertex(nrVertices, lastVertex);
+        for (int child = 0; child < nrVertices; child++) {
+            Edge<T> parentEdge = parentEdges[child];
+
+            if (parentEdge != null) {
+                int parent = parentEdge.getSource();
+                checkInvalidVertex(nrVertices, parent);
+
+                int destination = parentEdge.getDestination();
+                checkNotEqualDestinationChild(destination, child);
+            }
+        }
+
+        return getPathRecursive(parentEdges, lastVertex, 0);
     }
 
     private static <T extends Number> List<Edge<T>> getPathRecursive(
-    Edge<T>[] parentEdges, int child, int nrRecursiveCalls) {
+            Edge<T>[] parentEdges, int child, int pathLength) {
         if (parentEdges[child] == null) {
             List<Edge<T>> path = new LinkedList<>();
             return path;
         }
 
-        nrRecursiveCalls++;
-        if(nrRecursiveCalls < parentEdges.length) {
+        pathLength++;
+        if(pathLength < parentEdges.length) {
             Edge<T> parentEdge = parentEdges[child];
 
             int parent = parentEdge.getSource();
             List<Edge<T>> path = getPathRecursive(parentEdges, parent,
-                    nrRecursiveCalls);
+                    pathLength);
 
             path.add(parentEdge);
             return path;
         }
 
-        throw new IllegalArgumentException("A cycle has been detected");
+        throw new IllegalArgumentException("A cycle has been detected.");
     }
 
     private static void checkNullGraph(Graph graph) {
@@ -82,10 +98,17 @@ public class BFS {
         }
     }
 
-    private static void checkInvalidVertex(Graph graph, int vertex) {
-        if(vertex < 0 || vertex >= graph.getNrVertices()) {
+    private static void checkInvalidVertex(int nrVertices, int vertex) {
+        if(vertex < 0 || vertex >= nrVertices) {
             throw new IllegalArgumentException(String.format("Vertex %d does "
                     + "not exist in the graph.", vertex));
+        }
+    }
+
+    private static void checkNotEqualDestinationChild(int destination, int child) {
+        if(destination != child) {
+            throw new IllegalArgumentException(String.format("Destination must "
+                    + "be vertex %d, but is vertex %d.", destination, child));
         }
     }
 }
