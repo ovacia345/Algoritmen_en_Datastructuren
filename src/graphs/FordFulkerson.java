bf0e93f33c7fc51cd64ffa2e27b9e7d8a799f454
ovacia345 <nielsvann@hotmail.nl>
2018-10-24 12:03:59 +0200

    Add stub methods to FordFulkerson.java

--- src/graphs/FordFulkerson.java
+++ src/graphs/FordFulkerson.java
@@ -6,5 +6,12 @@ package graphs;
  * @author C Amghane
  */
 public class FordFulkerson {
+    public static <T extends Number> Graph<T> run(Graph<T> graph, int source,
+            int sink) {
+        
+    }
 
+    public static <T extends Number> T getFlowValue(Graph<T> flowGraph) {
+
+    }
 }
