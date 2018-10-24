a08f2e6ea00f2ab0c1208784577b3b6afa83ee0b
ovacia345 <nielsvann@hotmail.nl>
2018-10-24 17:10:05 +0200

    Add getPath() method to BFS.java

--- src/graphs/BFS.java
+++ src/graphs/BFS.java
@@ -48,12 +48,40 @@ public class BFS {
         return parentEdges;
     }
 
+    public static <T extends Number> List<Edge<T>> getPath(Edge<T>[] parentEdges,
+            int child) {
+        return getPathRecursive(parentEdges, child, 0);
+    }
+
+    private static <T extends Number> List<Edge<T>> getPathRecursive(
+    Edge<T>[] parentEdges, int child, int nrRecursiveCalls) {
+        if (parentEdges[child] == null) {
+            List<Edge<T>> path = new LinkedList<>();
+            return path;
+        }
+
+        nrRecursiveCalls++;
+        if(nrRecursiveCalls < parentEdges.length) {
+            Edge<T> parentEdge = parentEdges[child];
+
+            int parent = parentEdge.getSource();
+            List<Edge<T>> path = getPathRecursive(parentEdges, parent,
+                    nrRecursiveCalls);
+
+            path.add(parentEdge);
+            return path;
+        }
+
+        throw new IllegalArgumentException("A cycle has been detected");
+    }
+
     private static void checkNullGraph(Graph graph) {
         if(graph == null) {
             throw new IllegalArgumentException("A graph with a null pointer "
                     + "was given.");
         }
     }
+
     private static void checkInvalidVertex(Graph graph, int vertex) {
         if(vertex < 0 || vertex >= graph.getNrVertices()) {
             throw new IllegalArgumentException(String.format("Vertex %d does "
