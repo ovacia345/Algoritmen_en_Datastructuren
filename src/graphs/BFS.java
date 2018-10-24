1267aea60e62c3d7ac40a81017e2af28eb124d5b
ovacia345 <nielsvann@hotmail.nl>
2018-10-24 14:38:38 +0200

    Add stub methods to BFS.java

--- src/graphs/BFS.java
+++ src/graphs/BFS.java
@@ -3,7 +3,10 @@ package graphs;
 /**
  *
  * @author N.C.M. van Nistelrooij
+ * @author C Amghane
  */
 public class BFS {
-
+    public static <T extends Number> T[][] run(Graph<T> graph, int source) {
+        
+    }
 }
