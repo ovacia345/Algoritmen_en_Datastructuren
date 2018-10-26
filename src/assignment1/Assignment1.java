3d96c0f3845a16a8251c3fd45566671aec2cf042
ovacia345 <nielsvann@hotmail.nl>
2018-10-26 17:48:36 +0200

    Change Assignment1.java such that it works with new files

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -1,7 +1,5 @@
 package assignment1;
 
-import graphs.*;
-
 /**
  *
  * @author N.C.M. van Nistelrooij
@@ -19,34 +17,38 @@ public class Assignment1 {
             boxes[i] = box;
         }  
         
-        Graph<Integer> graph = createGraph(nrOfBoxes, boxes);
+        Graph graph = createGraph(nrOfBoxes, boxes);
 
-        FordFulkerson fordFulkerson = new FordFulkerson(1, 0);
+        long time = System.currentTimeMillis();
 
-        Graph<Integer> flowGraph = fordFulkerson.run(graph,
+        Graph flowGraph = FordFulkerson.run(graph,
                 0, graph.getNrOfVertices() - 1);
-        Integer flowValue = fordFulkerson.getFlowValue(flowGraph, 0);
+        int flowValue = FordFulkerson.getFlowValue(flowGraph, 0);
+
+        time = System.currentTimeMillis() - time;
+
+        IOHandler.print(Long.toString(time));
         
         IOHandler.print(Integer.toString(nrOfBoxes - flowValue));
     }
     
-    private static Graph<Integer> createGraph(int nrOfBoxes, Box[] boxes) {
+    private static Graph createGraph(int nrOfBoxes, Box[] boxes) {
         int nrOfVertices = nrOfBoxes * 2 + 2; // bipartite, +sink +source
-        Graph<Integer> graph = new Graph<>(nrOfVertices, 1); // source = souce, sink = sink;
+        Graph graph = new Graph(nrOfVertices); // source = souce, sink = sink;
         int source = 0;
         int sink = nrOfVertices-1;
         //source connections  
         for(int box  = 1 ; box <= nrOfBoxes; box++) {
-            graph.addEdge(source, box,1);
+            graph.addEdge(source, box);
         }
         //sink connections
         for(int box = sink - 1; box > nrOfBoxes ; box--){
-            graph.addEdge(box, sink,1);
+            graph.addEdge(box, sink);
         }
         for(int i = 0; i < nrOfBoxes; i++){
             for(int j=0; j < nrOfBoxes; j++){
                 if(i != j && boxes[i].fitsIn(boxes[j])) {
-                    graph.addEdge(i+1, j+1 + nrOfBoxes, 1);    // x number of boxes * 2 - nr of current box because of bipartite graph
+                    graph.addEdge(i+1, j+1 + nrOfBoxes);    // x number of boxes * 2 - nr of current box because of bipartite graph
                 }          
             }
         }
