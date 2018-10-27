e59f26a594462fc232087fb513f5e5e037d0b4b2
ovacia345 <nielsvann@hotmail.nl>
2018-10-27 11:25:44 +0200

    Add simplified BFS class to assignment1 package

---
+++ src/assignment1/BFS.java
@@ -0,0 +1,41 @@
+package assignment1;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ * @author N.C.M. van Nistelrooij
+ */
+public class BFS {
+    public static int[][] run(Graph graph, int source, int sink) {
+        int nrOfVertices = graph.getNrOfVertices();
+        boolean[] discovered = new boolean[nrOfVertices];
+        int[][] parentEdges = new int[nrOfVertices][4];
+
+        discovered[source] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+
+        while(!queue.isEmpty()) {
+            int vertexU = queue.remove();
+            List<int[]> adjacencyListVertexU = graph.getAdjacencyList(vertexU);
+
+            for(int[] edgeVariables : adjacencyListVertexU) {
+                int vertexV = edgeVariables[1];
+                if(discovered[vertexV] == false) {
+                    discovered[vertexV] = true;
+                    parentEdges[vertexV] = edgeVariables;
+                    queue.add(vertexV);
+
+                    if(discovered[sink] == true) {
+                        return parentEdges;
+                    }
+                }               
+            }           
+        }
+        
+        return parentEdges;
+    }
+}
